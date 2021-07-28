7d71892142c6b5ff2246df9895612a2a8f52321a
woodjellyfish <woodjellyfish1@gmail.com>
2021-07-28 15:46:22 +0900

    styled Button

--- components/timer/Button.tsx
+++ components/timer/Button.tsx
@@ -1,21 +1,15 @@
 import { MouseEventHandler } from "react";
+import styles from "./Button.module.scss";
 
 type Props = {
   name: string;
   onClick: MouseEventHandler<HTMLButtonElement>;
 };
 
-const colors = {
-  red: "mb-3 mx-1 rounded-full items-center shadow-lg bg-red-500 px-4 py-2 text-gray-100 font-bold text-xl md:hover:bg-red-400 select-none",
-  blue: " mb-3 mx-1 rounded-full items-center shadow-lg bg-blue-500 px-4 py-2 text-gray-100 font-bold text-xl md:hover:bg-blue-400 select-none",
-  green:
-    "mb-3 mx-1 rounded-full items-center shadow-lg bg-green-500 px-4 py-2 text-gray-100 font-bold text-xl md:hover:bg-green-400 select-none",
-};
-
 const Button = ({ name, onClick }: Props) => {
-  const color = name === "stop" ? "red" : "blue";
+  const color = name === "stop" ? styles.btnStop : styles.btnPrimary;
   return (
-    <button className={colors[color]} onClick={onClick}>
+    <button className={color} onClick={onClick}>
       {name}
     </button>
   );
