4109f260ec252e358192898dfe85e2ad2287dfe7
woodjellyfish <woodjellyfish1@gmail.com>
2021-07-29 19:28:36 +0900

    fix modal

--- components/comment/ConfModal.tsx
+++ components/comment/ConfModal.tsx
@@ -1,52 +1,25 @@
 import React from "react";
 
 type Props = {
-  userName: string;
-  commentMessage: string;
-  setIsModalOpen: (value: React.SetStateAction<boolean>) => void;
-  setIsVerification: (value: React.SetStateAction<boolean>) => void;
+  handleCancelClick: () => void;
 };
 
-export default function VerificationModal({
-  userName,
-  commentMessage,
-  setIsModalOpen,
-  setIsVerification,
-}: Props) {
-  const handleOKonClick = () => {
-    setIsVerification(true);
-    setIsModalOpen(false);
-  };
-  const handleCancelClick = () => {
-    setIsVerification(false);
-    setIsModalOpen(false);
-  };
+const ConfModal: React.FC<Props> = ({ children, handleCancelClick }) => {
   return (
-    <div
-      className="fixed top-0 left-0 h-full w-full bg-gray-500 bg-opacity-40 flex items-center justify-center"
-      onClick={handleCancelClick}
-    >
+    <div className="w-full flex justify-center absolute z-10">
       <div
-        className="w-[80%] md:w-1/3 bg-white rounded-lg py-8 shadow-lg flex flex-col items-center justify-center"
-        onClick={(e) => e.stopPropagation()}
+        className="fixed top-0 left-0 h-full w-full bg-gray-500 bg-opacity-40 flex items-center justify-center"
+        onClick={handleCancelClick}
       >
-        <div>ユーザー名</div>
-        <div>{userName}</div>
-        <div>コメント内容</div>
-        <div>{commentMessage}</div>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6"
-          onClick={handleOKonClick}
+        <div
+          className="w-[80%] md:w-1/3 bg-white rounded-lg py-8 shadow-lg flex flex-col items-center justify-center"
+          onClick={(e) => e.stopPropagation()}
         >
-          OK
-        </button>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6"
-          onClick={handleCancelClick}
-        >
-          CANCEL
-        </button>
+          {children}
+        </div>
       </div>
     </div>
   );
-}
+};
+
+export default ConfModal;
--- components/comment/CreateCommentForm.tsx
+++ components/comment/CreateCommentForm.tsx
@@ -84,58 +84,95 @@ export default function CreateCommentForm({ postId, setCommentData }: Props) {
     }
   }, [isModalOpen]);
 
+  //モーダル用イベントハンドラとモーダルのボディコンポーネント
+  const handleOKonClick = () => {
+    setIsVerification(true);
+    setIsModalOpen(false);
+  };
+  const handleCancelClick = () => {
+    setIsVerification(false);
+    setIsModalOpen(false);
+  };
+
+  const ModalBody = () => {
+    return (
+      <>
+        <div className="">ユーザー名</div>
+        <div className="px-3 py-1 border-4 border-gray-300 rounded-lg">
+          {userName}
+        </div>
+        <div>コメント内容</div>
+        <div className="px-3 py-1 border-4 border-gray-300 rounded-lg">
+          {commentMessage}
+        </div>
+        <div>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6 mx-8"
+            onClick={handleOKonClick}
+          >
+            OK
+          </button>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6"
+            onClick={handleCancelClick}
+          >
+            CANCEL
+          </button>
+        </div>
+      </>
+    );
+  };
+
   return (
     <>
       <div className="w-full">
         {isModalOpen && (
-          <div className="w-full flex justify-center absolute z-10">
-            <ConfModal
-              userName={userName}
-              commentMessage={commentMessage}
-              setIsModalOpen={setIsModalOpen}
-              setIsVerification={setIsVerification}
-            />
-          </div>
+          <ConfModal handleCancelClick={handleCancelClick}>
+            <ModalBody />
+          </ConfModal>
         )}
-        <div className="bg-blue-300 p-2 rounded-md shadow-md">
-          <div className="mb-4">
-            <label className="block text-gray-600 text-sm font-semibold mb-2">
-              ユーザー名
-            </label>
-            <input
-              className="bg-gray-100 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring focus:border-blue-600"
-              id="userName"
-              type="text"
-              placeholder="ユーザー名"
-              value={userName}
-              onChange={(e) => handleChangeName(e)}
-            />
-            <a className="ml-1.5 text-red-600">{userNameErrorMessage}</a>
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-600 text-sm font-semibold mb-2">
-              コメント内容
-            </label>
-            <textarea
-              rows={4}
-              cols={50}
-              className="bg-gray-100 p-1 appearance-none border rounded w-full text-gray-700 leading-tight focus:outline-none focus:ring focus:border-blue-600"
-              id="commentMessage"
-              onChange={(e) => handleChangeCommentMessage(e)}
-              value={commentMessage}
-            />
-            <a className="ml-1.5 text-red-600">{commentErrorMessage}</a>
-          </div>
+      </div>
 
-          <div className="flex items-center justify-between">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:border-blue-700"
-              type="button"
-              onClick={handleSubmitCommentData}
-            >
-              コメントする
-            </button>
-          </div>
+      <div className="bg-blue-300 p-2 rounded-md shadow-md">
+        <div className="mb-4">
+          <label className="block text-gray-600 text-sm font-semibold mb-2">
+            ユーザー名
+          </label>
+          <input
+            className="bg-gray-100 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring focus:border-blue-600"
+            id="userName"
+            type="text"
+            placeholder="ユーザー名"
+            value={userName}
+            onChange={(e) => handleChangeName(e)}
+            onFocus={() => setUserNameErrorMessage("")}
+          />
+          <a className="ml-1.5 text-red-600">{userNameErrorMessage}</a>
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-600 text-sm font-semibold mb-2">
+            コメント内容
+          </label>
+          <textarea
+            rows={4}
+            cols={50}
+            className="bg-gray-100 p-1 appearance-none border rounded w-full text-gray-700 leading-tight focus:outline-none focus:ring focus:border-blue-600"
+            id="commentMessage"
+            onChange={(e) => handleChangeCommentMessage(e)}
+            value={commentMessage}
+            onFocus={() => setCommentErrorMessage("")}
+          />
+          <a className="ml-1.5 text-red-600">{commentErrorMessage}</a>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:border-blue-700"
+            type="button"
+            onClick={handleSubmitCommentData}
+          >
+            コメントする
+          </button>
         </div>
       </div>
     </>
