106daf88f6d6c34be85b016215237ee843584442
woodjellyfish <woodjellyfish1@gmail.com>
2021-08-05 04:05:56 +0900

    refactoring

--- pages/api/sendmail.ts
+++ pages/api/sendmail.ts
@@ -4,14 +4,13 @@ import { cache } from "swr";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const apiKey = process.env.SENDGRID_SERVICE_KEY;
-  const fromEmail = process.env.SENDGRID_SERVICE_EMAIL;
-  //   const fromName = process.env.SENDGRID_SERVICE_NAME;
+  const fromEmail = "woodjellyfish1@gmail.com";
 
   const { postId, commentMessage, userName } = req.body;
 
   sendgrid.setApiKey(apiKey);
   const message = {
-    to: ["woodjellyfish1@gmail.com", "hirosi10893@gmail.com"],
+    to: "woodjellyfish1@gmail.com",
     from: `自動メール<${fromEmail}>`,
     subject: `${postId}にコメントが投稿されました。`,
     text: `投稿者: ${userName}\n投稿内容:${commentMessage}`,
